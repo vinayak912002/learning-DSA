d7b7044bdb290ba42b7d5515b565b23f3dbb65eb
Vinayak Pandey <vinayak912002@gmail.com>
2023-08-22 12:28:47 +0530

    added insertion sort algorithm

---
+++ src/sorting_algorithms/Insertion_sort.java
@@ -0,0 +1,42 @@
+package sorting_algorithms;
+
+import java.util.Arrays;
+
+public class Insertion_sort {
+    // In insertion sort the leftmost element in each pass is put at its correct place.
+    // Step 1 : [5,3],4,2,1 ==> 3,5,4,2,1
+    // Step 2 : [3,5,4],2,1 ==> 3,4,5,2,1
+    // Step 3 : [3,4,5,2],1 ==> 2,3,4,5,1
+    // Step 4 : [2,3,4,5,1] ==> 1,2,3,4,5 and the list is sorted
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[second];
+        arr[second] = arr[first];
+        arr[first] = temp;
+    }
+//basically there are two pointers i and j
+    //the i pointer keeps track of the size of the array in which we are currently working
+    //the j pointer helps us to sort the array
+    static void insertionSortAsc(int[] nums){
+        for (int i = 1; i < nums.length; i++){
+            for (int j = i; j > 0; j--){
+                if(nums[j - 1] > nums[j]) swap(nums, j-1, j);
+            }
+        }
+    }
+    static void insertionSortDesc(int[] nums){
+        for (int i = 1; i < nums.length; i++){
+            for (int j = i; j > 0; j--){
+                if(nums[j - 1] < nums[j]) swap(nums, j-1, j);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5,4,3,2,1};
+        int[] nums2 = {1,2,3,4,5};
+        insertionSortAsc(nums);
+        insertionSortDesc(nums2);
+        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString(nums2));
+    }
+}
